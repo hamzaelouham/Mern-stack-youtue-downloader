2af81b48505a93fef0fbd675d1732450fb9a0799
hamza elouham <hamzaelouham@gmail.com>
2022-08-10 12:24:33 +0100

    nothing to commit

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,10 @@ const {
   getInfoController,
 } = require("../controllers/downloadController.js");
 
-const ApiRoute = (app) => {
-  app.get("/info/download", (req, res) => getInfoController(req, res));
+const ApiRoute = (route) => {
+  route.get("/info/download", (req, res) => getInfoController(req, res));
 
-  app.get("/downloads", (req, res) => downloadController(req, res));
+  route.get("/downloads", (req, res) => downloadController(req, res));
 };
 
 module.exports = ApiRoute;
